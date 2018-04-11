ddc31b3834fabea423ccb2baf2d2f8143e7ba8eb
donallen <da34773@ravens.racc.edu>
2018-04-11 17:47:08 -0400

    Added quotes.js file to use to build the sidebar menu and content for the main display area.  We modified the applicationInit function to sort the array.  Modified the menuBuilder function to build the menu on the array.  Modified the linkClicked function to display the quote when the side menu item is clicked.  We made a few css entries and cleaned up the index file.

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,7 +45,7 @@ function initializeApplication() {
 	elFooter.className = 'animated bounceInRight';
 
 	elMain.innerHTML += '<div style="width: 40%; margin: auto; padding:10px; margin-bottom: 20px;"><div class="progress" style="height: 20px;"><div id="loaderProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
-
+	quotArr = quotArr.sort((a, b) => a[1].localeCompare(b[1]));
 	displayPB();
 }
 
@@ -63,15 +63,14 @@ function displayPB() {
 	} else {
 		timerCount = 0;
 
-		//start step-402
+
 		document.body.innerHTML = formLogin();
-		//end step-402
 
 		return false;
 	}
 }
 
-//start step-402.0
+
 function formLogin() {
 	return '<form id="loginFrm" name="loginFrm" action="#" class="animated zoomIn"><div class="form-group"><label for="email">Email address:</label><input type="email" class="form-control" id="email" value="me@there.com"></div><div class="form-group"><label for="pwd">Password:</label><input type="password" class="form-control" id="pwd" value="dfdf"></div><div class="form-check"><label class="form-check-label"><input class="form-check-input" type="checkbox">Remember me </label></div><button id="loginBtn" type="submit" class="btn btn-primary"onclick="validateLogin()">Submit</button></form>';
 }
@@ -88,9 +87,6 @@ function validateLogin() {
 	return false;
 }
 
-//end step-402.0
-
-//start step-403-0
 function applicationUserInterface() {
 	let header = '<div class="container-fluid"><div class="row"><div class="col-12"><nav id="header" class="navbar navbar-dark fixed-top bg-dark"><a class="navbar-brand" data-dest="logoLink" href="#">inside out</a></nav></div></div>';
 	let main = '<div class="row contStage"><div class="col-2">' + buildMenu() + '</div><div class="col-10">' + buildMain() + '</div></div>';
@@ -105,7 +101,16 @@ function applicationUserInterface() {
 }
 
 function buildMenu() {
-	return '<nav class="sidebar animated slideInLeft"><ul class="nav flex-column"><li class="nav-item"><a class="nav-link active" data-dest="link01" href="#">Active</a></li><li class="nav-item"><a class="nav-link" data-dest="link02" href="#">Link</a></li><li class="nav-item"><a class="nav-link" data-dest="link03" href="#">Link</a></li><li class="nav-item"><a class="nav-link disabled" data-dest="link04" href="#">Disabled</a></li></ul></nav>';
+	
+	let sm = '<nav class="sidebar animated slideInLeft"><ul class="nav flex-column">';
+	
+	for (let i = 0; i < quotArr.length; i++) {
+		sm += '<li class="nav-item"><a class="nav-link active" data-dest="' + i + '" href="#">' + quotArr[i][1].split(",", 1) + '</a></li>';
+	}
+	
+	sm += '</ul></nav>';
+	
+	return sm;
 }
 
 function buildMain() {
@@ -114,8 +119,6 @@ function buildMain() {
 
 function linkClicked(obj) {
 	console.log(obj.dataset.dest);
-	var str = '<h1 class="animated zoomIn">' + obj.dataset.dest + ' was clicked!</h1>';
-	document.getElementById("main").innerHTML = str.repeat(20);
+	var str = '<div class="infoDiv"><h1 class="animated zoomIn">' + quotArr[obj.dataset.dest][0] + '<h1><div class="animated slideInRight auth">- ' + quotArr[obj.dataset.dest][1] + '</div></div>';
+	document.getElementById("main").innerHTML = str;
 }
-
-//end step-403-0
